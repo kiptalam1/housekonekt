caf4e3347c5ca75ec9ac8b61647daaf19115bd9a
kiptalam1 <adamskiptalam0@gmail.com>
2025-10-08 14:32:48 +0300

    feat(property): implement functionality to fetch all property listings. - added filtering abilities through req.query - included metadata for pagination. - sorted propert by status and createdAt respectively. - added an endpoint for fetching to routes folder.

--- apps/backend/src/controllers/property.controllers.ts
+++ apps/backend/src/controllers/property.controllers.ts
@@ -1,4 +1,5 @@
-import type { Response } from "express";
+import { Prisma } from "../../generated/prisma/client.js";
+import type { Request, Response } from "express";
 import type { AuthenticatedRequest } from "../middlewares/auth.middlewares.js";
 import {
 	PrismaClient,
@@ -91,6 +92,100 @@ export const createProperty = async (
 	} catch (error) {
 		if (isDev) {
 			console.log("Error in createProperty", error);
+		}
+		return res.status(500).json({ error: "Internal server error" });
+	}
+};
+
+type PropertyQueryTypes = {
+	page?: string;
+	limit?: string;
+	location?: string;
+	type?:
+		| "BEDSITTER"
+		| "STUDIO"
+		| "ONE_BEDROOM"
+		| "TWO_BEDROOM"
+		| "THREE_BEDROOM"
+		| "MAISONETTE"
+		| "TOWNHOUSE"
+		| "BUNGALOW"
+		| "SERVICED_APARTMENT"
+		| "VILLA";
+	minPrice?: string;
+	maxPrice?: string;
+	status?: "AVAILABLE" | "FULL";
+};
+
+export const getAllProperty = async (
+	req: Request<{}, {}, {}, PropertyQueryTypes>,
+	res: Response
+): Promise<Response | void> => {
+	try {
+		const { page, limit, location, type, minPrice, maxPrice, status } =
+			req.query;
+		const pageNum = parseInt(page ?? "1", 10);
+		const limitNum = parseInt(limit ?? "20", 10);
+		const skip = (pageNum - 1) * limitNum;
+
+		const filters: Prisma.PropertyWhereInput = {};
+		if (location) {
+			filters.location = { contains: location, mode: "insensitive" };
+		}
+
+		if (type) {
+			filters.type = type;
+		}
+		if (status) {
+			filters.status = status;
+		}
+		if (minPrice || maxPrice) {
+			filters.price = {};
+			if (minPrice) {
+				filters.price.gte = Number(minPrice);
+			}
+			if (maxPrice) {
+				filters.price.lte = Number(maxPrice);
+			}
+		}
+
+		const property = await prisma.property.findMany({
+			skip,
+			take: limitNum,
+			where: filters,
+			omit: {
+				ownerId: true,
+				updatedAt: true,
+				views: true,
+				description: true,
+				units: true,
+			},
+			orderBy: [
+				{
+					status: "asc",
+				},
+				{
+					createdAt: "desc",
+				},
+			],
+		});
+
+		const totalProperty = await prisma.property.count({ where: filters });
+		const totalPages = Math.max(1, Math.ceil(totalProperty / limitNum));
+
+		return res.status(200).json({
+			meta: {
+				page: pageNum,
+				skip,
+				limit: limitNum,
+				totalProperty,
+				totalPages,
+			},
+			data: property,
+		});
+	} catch (error) {
+		if (isDev) {
+			console.error("Error fetching all property", error);
 		}
 		return res.status(500).json({ error: "Internal server error" });
 	}
--- apps/backend/src/routes/property.routes.ts
+++ apps/backend/src/routes/property.routes.ts
@@ -3,7 +3,10 @@ import {
 	isAdminOrOwner,
 	verifyAuthenticationToken,
 } from "../middlewares/auth.middlewares.js";
-import { createProperty } from "../controllers/property.controllers.js";
+import {
+	createProperty,
+	getAllProperty,
+} from "../controllers/property.controllers.js";
 import { validatePropertyCreationInput } from "../validators/property.validators.js";
 import { handleValidationErrors } from "../middlewares/validation.middleware.js";
 
@@ -17,5 +20,7 @@ router.post(
 	handleValidationErrors,
 	createProperty
 );
+
+router.get("/", getAllProperty);
 
 export default router;
