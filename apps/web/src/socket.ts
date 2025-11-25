398231ba98f2817ff22667e31e92399df3faf52d
kiptalam1 <adamskiptalam0@gmail.com>
2025-11-25 19:00:11 +0300

    fix: use the correct render backend url for socket in frontend

--- apps/web/src/socket.ts
+++ apps/web/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://housekonekt-backend.onrender.com", {
 	withCredentials: true,
 	autoConnect: false,
 });
